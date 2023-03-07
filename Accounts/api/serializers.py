8e09c3a6c64e098d20226ddbe0b7fb96f9462352
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2023-03-07 15:03:43 +0000

    Deleted stuff and added new stuff

---
+++ Accounts/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+
+from Account.models import Account
+
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
